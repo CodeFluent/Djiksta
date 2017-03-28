0832466bd5d5f589fe5530c66ffc5497496c7532
Wasfi Momen <wasfimomenwim@gmail.com>
2017-03-28 13:13:23 -0400

    tried

---
+++ src/main/company/Main.java
@@ -0,0 +1,20 @@
+package company;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * Created by Wasfi on 3/28/2017.
+ */
+
+public
+
+public class Main {
+
+    public static void main (String [] args) {
+
+
+
+    }
+
+}
